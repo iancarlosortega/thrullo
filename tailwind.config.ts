ccdf8c7e2eb0a75787f2b6882b9cdbafc7523734
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-10-05 09:11:43 -0500

    Project initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				'light-gray': '#F8F9FD',
+				dark: '#333333',
+				primary: '#2F80ED',
+				'primary-lt': '#DAE4FD',
+				secondary: '#828282',
+				'secondary-lt': '#BDBDBD',
+				'secondary-lts': '#F2F2F2',
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
